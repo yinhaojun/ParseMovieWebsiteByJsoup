45356e974402b7b738fed36aa9d11fb9ee440811
尹君子 <858032007@qq.com>
2016-05-30 13:48:36 +0800

    去主页的时候关闭免责申明

--- app/src/main/java/cn/tek/udo/parsemoviewebsitebyjsoup/view/activity/ReliefActivity.java
+++ app/src/main/java/cn/tek/udo/parsemoviewebsitebyjsoup/view/activity/ReliefActivity.java
@@ -56,6 +56,7 @@ public class ReliefActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(ReliefActivity.this, MainActivity.class));
+                finish();
             }
         });
     }
